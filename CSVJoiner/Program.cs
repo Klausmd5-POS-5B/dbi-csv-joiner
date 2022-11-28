cae30a400952df56d8be29334a28a68321c3e586
Klausmd5 <klaus.scheiboeck@vortexdata.net>
2022-11-28 13:14:55 +0100

    fix 2013er file

--- CSVJoiner/Program.cs
+++ CSVJoiner/Program.cs
@@ -6,7 +6,7 @@
     var content = File.ReadAllLines(filePath);
 
     var name = filePath.Split(".csv")[0];
-    int mtyLines = int.Parse(name.Substring(name.Length -2)) <= 13 ? 1 : 2;
+    int mtyLines = int.Parse(name.Substring(name.Length -2)) > 13 ? 1 : 2;
     
     var header1 = content.Skip(mtyLines).Take(1).First().Split(',').ToList();
     var header2 = content.Skip(mtyLines+1).Take(1).First().Split(',').ToList();
@@ -20,7 +20,7 @@
         return x1.First.Replace("-", "") + " " + x1.Second;
     }).ToList();
     
-    var newContent = content.Skip(2).ToList();
+    var newContent = content.Skip(mtyLines+1).ToList();
     newContent[0] = String.Join(",", newHeader);
     
     var newFile = filePath.Replace(".csv", "")+"_new.csv";
